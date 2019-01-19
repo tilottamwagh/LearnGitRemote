f4231d5b8d6def95de30dede7b8737ea6de285b5
Tilottam <tilottamwagh@gmail.com>
2019-01-19 07:23:43 +0000

    change in addition

---
+++ calc.py
@@ -0,0 +1,25 @@
+
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#=============================================================
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+#=====================================================================
