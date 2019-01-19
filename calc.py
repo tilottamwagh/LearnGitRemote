f348048d9d662ac59c572c6359115647684e494b
Tilottam <tilottamwagh@gmail.com>
2019-01-19 07:45:08 +0000

    This is in B1 commit

--- calc.py
+++ calc.py
@@ -22,4 +22,5 @@ sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('This is new file')
 #=====================================================================
